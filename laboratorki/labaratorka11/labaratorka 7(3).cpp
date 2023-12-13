b7c51fda5cf7122fbfe7121b42cc8dd9d0e21725
GRobik11 <153455150+GRobik11@users.noreply.github.com>
2023-12-13 23:49:18 +0200

    Add files via upload

---
+++ laboratorki/labaratorka11/labaratorka 7(3).cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int n, m;
+
+    std::cout << "Введите количество строк (n): ";
+    std::cin >> n;
+
+    std::cout << "Введите количество столбцов (m): ";
+    std::cin >> m;
+
+    int** matrix = new int* [n];
+
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[m];
+    }
+
+    for (int i = 0; i < n; i++) { // Ввод элементов матрицы
+        for (int j = 0; j < m; j++) {
+            std::cout << "Введите элемент матрицы [" << i + 1 << "][" << j + 1 << "]: ";
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    int minColumn = 0;
+    int minElement = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {  // Нахождение столбца с наименьшим элементом
+            if (matrix[i][j] < minElement) {
+                minElement = matrix[i][j];
+                minColumn = j;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) { // Замена отрицательных элементов столбца на 0
+        if (matrix[i][minColumn] < 0) {
+            matrix[i][minColumn] = 0;
+        }
+    }
+
+
+    std::cout << "Измененная матрица:\n";
+    for (int i = 0; i < n; i++) { // Вывод измененной матрицы
+        for (int j = 0; j < m; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+    return 0;
+}
