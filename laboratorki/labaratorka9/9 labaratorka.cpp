29c3b6f4dd6bf423b0abe09b0458eb326215c03f
GRobik11 <153455150+GRobik11@users.noreply.github.com>
2023-12-13 23:47:59 +0200

    Add files via upload

---
+++ laboratorki/labaratorka9/9 labaratorka.cpp
@@ -0,0 +1,24 @@
+﻿#include "functions.hpp"
+#include <iostream>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int n;
+    bool flag;
+    int matrix[100][100];
+
+    std::cout << "Введите какого порядка матрица: ";
+    std::cin >> n;
+
+    jp::Read(matrix, n);
+
+    flag = jp::Check_3or5(matrix, n);
+    if (flag == true) {
+        std::cout << "Прошло" << flag;
+        jp::changeMatrix(matrix, n);
+    }
+    jp::Write(matrix, n);
+}
+
